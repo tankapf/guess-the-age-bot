cecb10ea21c049caafd74317ac414a55cde9c505
tankapf <107043192+tankapf@users.noreply.github.com>
2022-06-13 16:40:28 +0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "5485807434:AAGOl1bAoAuEqPn9L5cjS_eBsjcW4eH-IOs",
+	token: "5485807434-IOs",
 	rounds: 5, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 3, //timer indicators
